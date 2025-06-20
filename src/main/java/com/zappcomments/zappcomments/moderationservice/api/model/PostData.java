02c36aa4f4337e384557357d5777cb69606158d6
tzappiello <tzappiello@gmail.com>
2025-06-20 10:02:54 -0300

    Refatoracao dos dtos

--- src/main/java/com/zappcomments/zappcomments/moderationservice/api/model/PostData.java
+++ src/main/java/com/zappcomments/zappcomments/moderationservice/api/model/PostData.java
@@ -3,13 +3,11 @@ package com.zappcomments.zappcomments.moderationservice.api.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @Builder
 public class PostData {
 
-    private String id;
+    private String postId;
     private Integer wordCount;
     private Double calculatedValue;
 }
